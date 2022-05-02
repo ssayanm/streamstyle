9a96e4fc943049eba5cbe429de6a412e2534a182
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-05-02 09:36:31 +0530

    updated changes

--- pages/terms.js
+++ pages/terms.js
@@ -1,14 +1,17 @@
 const terms = () => {
   return (
     <div>
-      {/* <div name="termly-embed" data-id="0e8bac58-ea9f-4992-b6b4-db95e3d80dc5" data-type="iframe"></div>
+     <div name="termly-embed" data-id="0e8bac58-ea9f-4992-b6b4-db95e3d80dc5" data-type="iframe"></div>
     <script type="text/javascript">(function(d, s, id) {
       var js, tjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
       js.src = "https://app.termly.io/embed-policy.min.js";
       tjs.parentNode.insertBefore(js, tjs);
-    }(document, 'script', 'termly-jssdk'));</script>*/}
+    }(document, 'script', 'termly-jssdk'));
+    </script>
+
+    
     </div>
   );
 };
