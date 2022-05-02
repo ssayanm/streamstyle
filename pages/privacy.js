5d8736821eaf560fb9541ef3b076d38120b4b3d3
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-05-02 09:54:12 +0530

    removed unwanted code

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,19 +1,4 @@
 const privacy = () => {
-  return (
-    <div>
-      <div
-        name="termly-embed"
-        data-id="8f754f55-a0a9-4e6a-ac0f-7f5992f9b270"
-        data-type="iframe"
-      ></div>
-      {/* <script type="text/javascript">(function(d, s, id) {
-      var js, tjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "https://app.termly.io/embed-policy.min.js";
-      tjs.parentNode.insertBefore(js, tjs);
-    }(document, 'script', 'termly-jssdk'));</script>*/}
-    </div>
-  );
+  return <div>privacy</div>;
 };
 export default privacy;
--- pages/terms.js
+++ pages/terms.js
@@ -1,18 +1,4 @@
 const terms = () => {
-  return (
-    <div>
-     <div name="termly-embed" data-id="0e8bac58-ea9f-4992-b6b4-db95e3d80dc5" data-type="iframe"></div>
-    <script type="text/javascript">(function(d, s, id) {
-      var js, tjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "https://app.termly.io/embed-policy.min.js";
-      tjs.parentNode.insertBefore(js, tjs);
-    }(document, 'script', 'termly-jssdk'));
-    </script>
-
-    
-    </div>
-  );
+  return <div>terms</div>;
 };
 export default terms;
