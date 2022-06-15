b7e57f97a1969ea78eddb6affab329d86917ecf4
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-06-15 08:03:50 +0530

    added a static homepage

---
+++ pages/home.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import Meta from "../components/Meta";
+import Loading from "../components/Loading";
+import HomeStaticInfo from "../components/HomeStaticInfo";
+import HomeStaticHero from "../components/HomeStaticHero";
+
+export const getStaticProps = async () => {
+  try {
+    const res = await axios.get(`${process.env.url}/api/home/?populate=*`);
+    const res1 = await axios.get(
+      `${process.env.url}/api/how-it-works?populate=*`
+    );
+    const home = res.data;
+    const sales = res1.data;
+    if (!home && !sales)
+      return (
+        <div>
+          <Loading />
+        </div>
+      );
+    return { props: { home, sales } };
+  } catch (error) {
+    return { error };
+  }
+};
+
+const Home = ({ home, sales }) => {
+  return (
+    <>
+      <Meta title="Home" />
+      <HomeStaticHero home={home} />
+      <HomeStaticInfo sales={sales} />
+    </>
+  );
+};
+
+export default Home;
