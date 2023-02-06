cec7711522409c2dc63be67970b39a2928283d3b
Sayan Mukherjee <hello@sayanmukherjee.com>
2023-02-06 22:04:17 +0530

    added script

---
+++ pages/popstream.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Script from "next/script";
+
+const popstream = () => {
+  return (
+    <div>
+      <Script src="https://assets.revovideo.com/v1.0/embed.js" />
+    </div>
+  );
+};
+
+export default popstream;
